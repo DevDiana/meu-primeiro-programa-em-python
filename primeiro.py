cbaa9fbd5c022d057f484ea65b0027dbcfa6ad0c
diana <devdianaregina@gmail.com>
2020-09-29 22:18:47 -0300

    first commit

---
+++ primeiro.py
@@ -0,0 +1,19 @@
+a = 10
+b = 5
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+print('soma: ' + str(soma))
+#print('soma: ' + soma)
+print(soma)
+print(subtracao)
+print(multiplicacao)
+print(int(divisao))
+print(resto)
+
+#como converter uma string para inteiro
+x = '1'
+soma2 = int(x) + 1
+print(soma2)
