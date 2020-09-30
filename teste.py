f4af113fe471a9efd7dfba357535836e5097f13b
diana <devdianaregina@gmail.com>
2020-09-29 22:40:22 -0300

    enviando o arquivo de teste dos tipos primitivos

---
+++ teste.py
@@ -0,0 +1,11 @@
+#n = (input('Digite um valor: '))
+#print(n.isalpha())
+
+n1 = int(input('Digite um valor: '))
+n2 = int(input ('Digite outro valor: '))
+soma = n1 + n2
+
+print('A soma de {} e {} é {}'.format(n1, n2 , soma))
+
+
+
