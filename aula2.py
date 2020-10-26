6ea5b2307e4c0c561014d40e2bfba7661624d8b4
DevDiana <devdianaregina@gmail.com>
2020-10-25 22:36:08 -0300

    variaveis em python

---
+++ aula2.py
@@ -0,0 +1,10 @@
+a =  int(input('Entre com o primeiro valor: '))
+b = int(input('Entre com o segundo valor: '))
+soma = a + b
+subtracao = a - b
+multiplicacao = a * b
+divisao = a / b
+resto = a % b
+resultado = ('Soma: {}.  Subtração: {}.' '\n Multiplição: {}'.format(soma, subtracao, multiplicacao))
+
+print(resultado)
